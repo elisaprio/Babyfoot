ac6cd821213eadd66b88ab24b9a2238d98108ad1
Elisa Priollaud <elisaprio@live.fr>
2018-01-16 20:30:36 +0100

    creation des boutons à chaque ligne

--- babyfoot_EP/public/javascripts/onClickFunction.js
+++ babyfoot_EP/public/javascripts/onClickFunction.js
@@ -1,5 +1,6 @@
 let urlAll = "http://localhost:3000/all/";
 let urlNew = "http://localhost:3000/new/";
+let urlRemove = "http://localhost:3000/remove/";
 var encounters = [];
 // CREATE DYNAMIC TABLE.
 //var table = document.createElement("table");
@@ -12,18 +13,6 @@ window.addEventListener("load", loadJSONTable(urlAll),true);
 //document.getElementById("body").addEventListener("load",loadJSONTable(urlAll),true);
 //document.getElementById("newMatch").addEventListener("load",createTableFromJSON(),true);
 
-function loadJSONnewMatch(url){
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            var myArr = JSON.parse(this.responseText);
-            console.log(myArr);
-            loadJSONTable(urlAll);   
-        }
-    };
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send(); 
-}
 function loadJSONTable(url){
     console.log("JSON LOAD");
     var xmlhttp = new XMLHttpRequest();
@@ -48,6 +37,19 @@ function loadJSONTable(url){
     xmlhttp.send(); 
 };
 
+function loadJSONnewMatch(url){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var myArr = JSON.parse(this.responseText);
+            console.log(myArr);
+            loadJSONTable(urlAll);   
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send(); 
+}
+
 function newMatch(){
     var x = document.getElementById("frm1");
     var text = "";
@@ -65,7 +67,7 @@ function newMatch(){
     //newMatch(text1,text2);
     loadJSONnewMatch(urlNew+text1+"&"+text2);
     document.getElementById("matches").innerHTML = "Done";
-    
+    x.reset();
     
 };
 function showTable(){
@@ -75,25 +77,59 @@ function showTable(){
     divContainer.appendChild(table);
 };
 
+function loadJSONremove(url){
+    alert("Button");
+}
+
+function removeRow(){
+
+}
+
+//To add button to one row
+function addButton(index){
+    var button = document.createElement("button");
+    button.setAttribute('type', 'button');
+    button.setAttribute('value', "Remove");
+    var textnode = document.createTextNode("Remove");         // Create a text node
+    button.appendChild(textnode);
+    button.setAttribute("id","b"+index);
+    button.setAttribute('onclick', 'loadJSONremove(urlRemove)');
+    return button;
+}
+
+
 // ADD JSON DATA TO THE TABLE AS ROWS.
 function addRowsToTable(){
     for (var i = 0; i < encounters.length; i++) {
         tr = table.insertRow(-1);
-        for (var j = 0; j < col.length; j++) {
+        for (var j = 0; j < col.length-1; j++) {
             var tabCell = tr.insertCell(-1);
             tabCell.innerHTML = encounters[i][col[j]];
+            if(j===0){
+                var index = tabCell.textContent;
+            }
         }
+        //Button to remove row
+        var tabCell = tr.insertCell(-1);
+        var btn = addButton(index);
+        tabCell.appendChild(btn);        
     }
 };
 
 function addOneRowToTable(){
     console.log("NEW LINE");
     tr = table.insertRow(-1);
-    for (var j = 0; j < col.length; j++) {
+    for (var j = 0; j < col.length-1; j++) {
         var tabCell = tr.insertCell(-1);
         tabCell.innerHTML = encounters[encounters.length-1][col[j]];
-        table.appendChild(tabCell);
+        if(j===0){
+            var index = tabCell.textContent;
+        }
     }
+    //Button to remove row
+    var tabCell = tr.insertCell(-1);
+    var btn = addButton(index);
+    tabCell.appendChild(btn);
 }
 
 function createTableFromJSON() {
@@ -107,7 +143,7 @@ function createTableFromJSON() {
             }
         }
     }
-    // CREATE DYNAMIC TABLE.
+    col.push("");
     
     // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
     var tr = table.insertRow(-1);                   // TABLE ROW.
